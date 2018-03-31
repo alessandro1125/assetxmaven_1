846a8d7bff52b65c777f93b7becfaa190b6bebbf
alessandro1125 <36631243+alessandro1125@users.noreply.github.com>
2018-03-31 14:26:50 +0200

    1426

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -15,9 +16,15 @@ import javax.servlet.http.HttpServletResponse;
 )
 public class HelloServlet extends HttpServlet {
 
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+
+        RequestDispatcher view = req.getRequestDispatcher("login.jsp");
+        view.forward(req,resp);
+
         ServletOutputStream out = resp.getOutputStream();
         out.write("hello heroku".getBytes());
         out.flush();
